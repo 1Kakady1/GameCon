030d46c138d426ba72f1dd42a3e3666d36ae29b5
Anton <anton2016_96@mail.ru>
2018-10-15 18:07:08 +0300

    the end of work with media

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,46 @@
+
+  function windowSize(){
+    if ($(window).width() <= '540'){
+    	let url = "phone.html";
+		$(location).attr('href',url);
+    }
+}
+		$(window).on('load resize',windowSize);
+
+
 window.addEventListener("DOMContentLoaded", function(){
 	"use strict";
 
-let linkVideo = document.getElementsByClassName("playvideo"),
+let flagVideo = false;
+
+if(flagVideo == true){
+
+	let linkVideo = document.getElementsByClassName("playvideo"),
 	videoPlay = document.getElementsByClassName("video-play"),
 	myRe = new RegExp(/autoplay=1|&/gi);
 
-for (let i = 0; i < linkVideo.length; i++) {
-	
-	linkVideo[i].addEventListener("mouseover",function(){
-		 let symbol = videoPlay[i].src.indexOf("?") > -1 ? "&" : "?";
-		 	 videoPlay[i].src+= symbol + "autoplay=1";
-	});
+	for (let i = 0; i < linkVideo.length; i++) {
+		
+		linkVideo[i].addEventListener("mouseover",function(){
+			 let symbol = videoPlay[i].src.indexOf("?") > -1 ? "&" : "?";
+			 	 videoPlay[i].src+= symbol + "autoplay=1";
+		});
 
 
-	linkVideo[i].addEventListener("mouseout",function(){
-		 let symbol = videoPlay[i].src;
-		 	console.log(myRe);
-		 	symbol= symbol.replace(myRe,"");
-		 	console.log(symbol);
-		 	videoPlay[i].src= symbol;
-	});
+		linkVideo[i].addEventListener("mouseout",function(){
+			 let symbol = videoPlay[i].src;
+			 	console.log(myRe);
+			 	symbol= symbol.replace(myRe,"");
+			 	console.log(symbol);
+			 	videoPlay[i].src= symbol;
+		});
 
 
-	
+		
+	}
 }
+
+
 
 
 var wow = new WOW(
@@ -147,5 +164,4 @@ btnSearch.addEventListener("click", function(){
 
     });
   }
-
 });
