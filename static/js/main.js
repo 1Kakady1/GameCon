0c927217866770cc002e548bd65812641611a005
Anton <anton2016_96@mail.ru>
2018-11-03 23:35:24 +0300

    start page post-game (magp,com,new f)

--- static/js/main.js
+++ static/js/main.js
@@ -222,41 +222,4 @@ btnSearch.addEventListener("click", function(){
 	        container.hide("slow"); 
 	    }
 	});
-// параллах
-let $parallaxClass = $('.slider-img > div')[0].className,
-	$returnImgPr=0;
-	$parallaxClass= '.'+$parallaxClass;
-	function returnImg(a){
-		switch(a) {
-			case ".parallax-video":
-				return "video.png"
-				break;
-			case ".parallax-game":
-				return "game.png"
-				break;
-			case ".parallax-window":
-				return "Layer_39.png"
-				break;
-			case ".parallax-news":
-				return "news.png"
-				break;
-			case ".parallax-article":
-				return "article.png"
-				break;
-			case ".parallax-audio":
-				return "def.png"
-				break;
-			default:
-				return "def.png"
-				break;
-		}
-	}
-
-$returnImgPr = returnImg($parallaxClass);
-console.log($returnImgPr)
-$($parallaxClass).parallax({
-  	imageSrc: `./img/${$returnImgPr}`,
-  	bleed: 0,
-  	speed: 0.2
-  });
 });
